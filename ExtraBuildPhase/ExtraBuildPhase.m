a89b2a7ee7b27dce02a4b5f4e62f185c537d4be2
Norio Nomura <norio.nomura@gmail.com>
2016-02-29 19:32:14 +0900

    Update copyright year to 2016

--- ExtraBuildPhase/ExtraBuildPhase.m
+++ ExtraBuildPhase/ExtraBuildPhase.m
@@ -4,7 +4,7 @@
 //
 //  Created by 野村 憲男 on 11/7/15.
 //
-//  Copyright (c) 2015 Norio Nomura
+//  Copyright (c) 2016 Norio Nomura
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
